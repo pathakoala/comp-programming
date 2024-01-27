2e1abeaa058b6ed01ee607bb947b70619ec7d7be
koalap <aalokp0324@gmail.com>
2024-01-26 20:58:52 -0500

    photoshoot final

--- USACO-Bronze (Java)/photoshoot.java
+++ USACO-Bronze (Java)/photoshoot.java
@@ -10,7 +10,14 @@ public class photoshoot {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
         n = Integer.parseInt(r.readLine());
-        String str = r.readLine();
+        char[] order = r.readLine().toCharArray();
+        int count  = 0;
+        for(int i = n-2; i>=0; i-=2) {
+            if(order[i] != order[i+1] && (order[i] == 'G') == (count%2 == 0)) {
+                count++;
+            }
+        }
+        pw.print(count);
         
         r.close();
         pw.close();
